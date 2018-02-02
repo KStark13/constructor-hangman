3da4124874df56675d743c86bf2bf72d338e0dfd
Kate <kate.stark13@sbcglobal.net>
2018-02-02 17:48:00 -0600

    updates to letter.js

---
+++ letter.js
@@ -0,0 +1,26 @@
+/*Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+A string value to store the underlying character for the letter
+A boolean value that stores whether that letter has been guessed yet
+A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly*/
+
+function Letter(ltr) {
+	this.letter = ltr
+	this.appear = false
+	this.letterShow = function () {
+		//shows a blank//
+		if (this.letter == ' ') {
+			this.appear = true
+			return ' '
+		//shows _ //
+		} if (this.appear === false) {
+			return ' _ '
+		//shows the letter guessed//
+		} else {
+			return this.letter
+		}
+	}
+}
+
+module.exports = Letter
